e185df595f6518ec2f03741a7b528d21d95d81a9
VatanakChamroeun <ch.vatanak01@gmail.com>
2022-12-05 14:07:59 +0700

    fix: change header and footer style

--- src/datas/Introduction.js
+++ src/datas/Introduction.js
@@ -1,9 +1,9 @@
-export const navBarName = 'VC.'
+export const navBarName = 'VC </>'
 
 export const introductionData = {
   name: 'Vatanak Chamroeun',
-  position: 'Developer!',
-  description: `A Computer Science senior year student at CADT ~ Interested in developing applications.
+  position: 'Passionate Developer!',
+  description: `A Computer Science senior year student at CADT ~ Interested in developing mobile applications.
   Based in Phnom Penh, Cambodia📍`,
   cvUrl: '#',
 }
